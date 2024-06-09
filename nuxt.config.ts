504e8aac8b4b491d5f699b6ba0e05fb55a1f1f7f
Valijon <parliament888@gmail.com>
2024-06-10 03:38:58 +0500

    Настроил TW, добавил шрифт

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: [
+    [
+      '@nuxtjs/google-fonts',
+      {
+        families: {
+          'PT Sans': true,
+        },
+      },
+    ],
+  ],
 });
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="text-xl text-red-500 underline text-center mt-10">test test</h1>
+</template>
